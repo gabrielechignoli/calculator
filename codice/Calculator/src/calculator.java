864d43e8511399417504b23d785f3654e97893bf
LukePanzer <l.panzeri6@studenti.unibg.it>
2022-10-03 10:03:32 +0200

    Modifiche

--- codice/Calculator/src/calculator.java
+++ codice/Calculator/src/calculator.java
@@ -1,9 +1,96 @@
+import java.util.Scanner;
 
-public class calculator {
+public class calcolatrice {
+	
+	public static void main (String args[]) {
+		
+		//Variabli
+		
+		Scanner scelta = new Scanner(System.in);
+		Scanner input  = new Scanner(System.in);
+		double pnum, snum, risultato;
+		double sc;
+		
+		System.out.println("Quale operazione vuoi svolgere? ");
+		sc = scelta.nextDouble();
+		
+		
+		//Addizione
+		
+		if (sc == 1) 
+		{
+			
+			
+			
+			System.out.println("Inserisci il primo numero: ");
+		pnum = input.nextDouble(); //inserisco il numero nella variabile
+		
+		System.out.println("Inserisci il secondo numero: ");
+		snum = input.nextDouble();
+		
+		risultato= pnum + snum; //svolgo l'operazione con le variabili
+		
+		System.out.println("Il risultato dell' addizione è "+ risultato);
+			
+			
+		}
+		
+		//Sottrazione
+		
+		if(sc== 2)
+		{
+			System.out.println("Inserisci il primo numero: ");
+		pnum = input.nextDouble();
+		
+		System.out.println("Inserisci il secondo numero: ");
+		snum = input.nextDouble();
+		
+		risultato= pnum - snum;
+		
+		System.out.println("Il risultato della sottrazione è "+ risultato);
+			
+			
+		}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Moltiplicazione
+		
+		if(sc== 3)
+		{
+			System.out.println("Inserisci il primo numero: ");
+		pnum = input.nextDouble();
+		
+		System.out.println("Inserisci il secondo numero: ");
+		snum = input.nextDouble();
+		
+		risultato= pnum * snum;
+		
+		System.out.println("Il risultato della moltiplicazione è "+ risultato);
+			
+		}
+		
+		//Divisione
+		
+		if(sc== 4)
+		{
+			System.out.println("Inserisci il primo numero: ");
+		pnum = input.nextDouble();
+		
+		System.out.println("Inserisci il secondo numero: ");
+		snum = input.nextDouble();
+		
+		risultato= pnum / snum;
+		
+		System.out.println("Il risultato della divisione è "+ risultato);
+		
+		
+		}
+		
+		
+		return;
+		
+			
+		
 
+		
 	}
-
 }
